5d1b63ca52295f36c218fc578df655c343ac2f63
Tereza Smolová <framboiseart@email.cz>
2024-04-12 18:34:01 +0200

    Working in progress

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom'
-import { Header } from '../Header/header'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from '../Header/Header';
 
 export const App = () => (
-	<>
+  <>
     <Header />
-		<Outlet />
-	</>
-)
+    <Outlet />
+  </>
+);
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,23 @@
 import './style.css';
-import { Header } from '../../components/Header/header';
+import { Header } from '../../components/Header/Header';
 import { useState, useEffect } from 'react';
 import { Product } from '../../components/Product/Product';
 import { useParams, Link } from 'react-router-dom';
 
 export const HomePage = () => {
-  const [products, setProducts] = useState([])
-  const {id} = useParams
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('https://apps.kodim.cz/react-2/xxxmuck/products')
-      const data = await response.json()
-      console.log(data)
-      setProducts(data)
-    }
+      const response = await fetch(
+        'https://apps.kodim.cz/react-2/xxxmuck/products',
+      );
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
 
-    fetchProducts()
+    fetchProducts();
   }, []);
 
   if (products === null) {
@@ -26,21 +27,23 @@ export const HomePage = () => {
   return (
     <div className="container">
       <main>
-        <div className='hero'>
-        <h1 className='hero__title'>Aktuální nabídka</h1>
-          <p className='hero__desc'>Nejnovější prémiové produkty od předních českých designérů
+        <div className="hero">
+          <h1 className="hero__title">Aktuální nabídka</h1>
+          <p className="hero__desc">
+            Nejnovější prémiové produkty od předních českých designérů
             <br />
             Doprava zdarma až k vám domů, na cenu nehleďte.
           </p>
         </div>
-        <div className='products'>
-          {products && products.map(({id, name, image}) => {
-            return (
-              <Link to={`/product/${id}`} >
-                <Product id={id} key={id} name={name} image={image} />
-              </Link>
-            )
-          })}
+        <div className="products">
+          {products &&
+            products.map(({ id, name, image }) => {
+              return (
+                <Link to={`/product/${id}`}>
+                  <Product id={id} key={id} name={name} image={image} />
+                </Link>
+              );
+            })}
         </div>
       </main>
     </div>
